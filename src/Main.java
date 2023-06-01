4b5fe79c925c3298f5314f3fba19ddc706c6d945
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-05-31 21:42:35 -0400

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                // Purchase price is 120
+                double purchasePrice = 120;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
+                // Sales tax rate is 0.05 %
+                double salesTaxRate = 0.05;
+                double salesTax = purchasePrice * salesTaxRate;
+
+                // Output statement
+                System.out.println("The price of your purchase is: $" + purchasePrice);
+                System.out.println("The computed sales tax at 5% is: $" + salesTax);
+            }
 }
